2409a2ee3bd716b30d3d7f969b47917f54dcc206
Gufran5687342 <syedgufran2020@gmail.com>
2026-09-16 23:33:25 +0530

    bit operators

---
+++ bits.java
@@ -0,0 +1,15 @@
+public class bits {
+    public static void main(String args[]){
+        int n = 5;
+        int pos = 2;
+        int bitMask = 1 << pos;
+
+        if((bitMask & n)==0){
+            System.out.print("bit was zero");
+        }
+        else{
+            System.out.print("bit was one");
+        }
+    }
+    
+}
