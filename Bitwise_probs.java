71ea635995dc2fe7cc5109d7d509b1d35ca60c0c
Gufran5687342 <syedgufran2020@gmail.com>
2026-09-16 21:01:58 +0530

    bitwise mastery

---
+++ Bitwise_probs.java
@@ -0,0 +1,9 @@
+public class Bitwise_probs {
+    public static void main(String[] args) {
+        System.out.println("Q1 Result: " + ((4 << 1) & 7)); // Outputs 0
+        System.out.println("Q2 Result: " + ((12 >> 2) | 5)); // Outputs 7
+        System.out.println("Q3 Result: " + ((1 << 3) ^ 8)); // Outputs 0
+        System.out.println("Q4 Result: " + ((6 & 3) << 2)); // Outputs 8
+        System.out.println("Bitwise logic completely mastered by the GOAT!");
+    }
+}
