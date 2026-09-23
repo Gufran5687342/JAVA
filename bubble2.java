50c38c594057bfe969485ba7235b7b981f290b05
Gufran5687342 <syedgufran2020@gmail.com>
2026-09-24 00:46:48 +0530

    bubble sort 2

---
+++ bubble2.java
@@ -0,0 +1,22 @@
+public class bubble2{
+    public static void print_arr(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String args[]){
+        int arr[] = {45,12,85,32,10};
+        for(int i = 0; i < arr.length-1; i++){
+            for(int j = 0; j <arr.length-i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+
+                }
+            }
+        }
+        print_arr(arr);
+
+    }
+}
